9ba1a3555e5a4c7c3321d01f589d967d78df9c5d
Polina Yermolenko <polina.shvajukova@gmail.com>
2020-09-07 00:15:21 +0300

    Поправки после проверки ментора

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,68 @@ const infoIcons = document.querySelectorAll(".form__label-radio");
 const modal = document.querySelector(".modal");
 
 for (let i=0; i<infoIcons.length; i++) {
-  infoIcons[i].addEventListener("mouseover", () => {
+  infoIcons[i].addEventListener("mouseover", (evt) => {
     modal.classList.remove("visually-hidden");
+
+    if (document.body.clientWidth < 500) {
+      modal.style.right='8%';
+
+      if(evt.target.id === 'ind') {
+        modal.style.bottom='25%';
+      }
+
+      if(evt.target.id === "bus") {
+        modal.style.bottom="22%";
+      }
+
+      if(evt.target.id === "inv") {
+        modal.style.bottom="19%";
+      }
+
+      if(evt.target.id === "tr") {
+        modal.style.bottom="16%";
+      }
+      
+    } 
+    else if(document.body.clientWidth >= 500 && document.body.clientWidth < 1130) {
+      modal.style.bottom="17%";
+     
+      if(evt.target.id === "ind") {
+        modal.style.right="65%";
+      }
+     
+      if(evt.target.id === "bus") {
+        modal.style.right="37%";
+      }
+  
+      if(evt.target.id === "inv") {
+        modal.style.right="16%";
+      }
+  
+      if(evt.target.id === "tr") {
+        modal.style.right="2%";
+      }
+    } else {
+      modal.style.bottom="25%"
+
+      if(evt.target.id === "ind") {
+        modal.style.right="66%";
+      }
+     
+      if(evt.target.id === "bus") {
+        modal.style.right="38%";
+      }
+  
+      if(evt.target.id === "inv") {
+        modal.style.right="17%";
+      }
+  
+      if(evt.target.id === "tr") {
+        modal.style.right="3%";
+      }
+    }
+
+    
   });
 };
 
